df52cccf35783a7416d9f33a7e7c0bc17442c7c4
DimasHN_04 <dimashn04@gmail.com>
2024-02-02 00:11:47 +0700

    Implemented Product Controller

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -0,0 +1,32 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import id.ac.ui.cs.advprog.eshop.service.ProductService;
+
+@Controller
+@RequestMapping("/product")
+public class ProductController {
+    @Autowired
+    private ProductService service;
+
+    @GetMapping("/create")
+    public String createProductPage(Model model) {
+        Product product = new Product();
+        model.addAttribute("product", product);
+        return "createProduct";
+    }
+
+    public String productListPage(Model model) {
+        List<Product> products = service.findAll();
+        model.addAttribute("products", products);
+        return "productList";
+    }
+}
