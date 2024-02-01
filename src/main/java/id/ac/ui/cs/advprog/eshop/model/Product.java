646f8a3afb8dcc20980ad84dfed723ef648a1d1b
DimasHN_04 <dimashn04@gmail.com>
2024-02-01 23:56:32 +0700

    Added Product.java

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -0,0 +1,10 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class Product {
+    private String productId;
+    private String productName;
+    private int productQuantity;
+}
