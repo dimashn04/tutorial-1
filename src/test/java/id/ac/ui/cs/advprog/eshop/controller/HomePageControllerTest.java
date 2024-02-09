ef4c4bfc70002bec8160d11adb622138ada39a4a
DimasHN_04 <dimashn04@gmail.com>
2024-02-09 22:57:57 +0700

    Added HomePageControllerTest.java

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/HomePageControllerTest.java
@@ -0,0 +1,25 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(HomePageController.class)
+public class HomePageControllerTest {
+    
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void testHomePage() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+               .andExpect(MockMvcResultMatchers.status().isOk())
+               .andExpect(MockMvcResultMatchers.view().name("HomePage"));
+    }
+}
