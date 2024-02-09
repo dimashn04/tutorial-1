24b7e9644b56215993d2dfb8cc24725ef7cecf22
DimasHN_04 <dimashn04@gmail.com>
2024-02-09 22:30:49 +0700

    Product Service Unit Test added

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImplTest.java
@@ -0,0 +1,116 @@
+package id.ac.ui.cs.advprog.eshop.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import id.ac.ui.cs.advprog.eshop.repository.ProductRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class ProductServiceImplTest {
+    @Mock
+    ProductRepository productRepository;
+
+    @InjectMocks
+    ProductServiceImpl productServiceImpl;
+
+    @BeforeEach
+    void setup() {}
+
+    @Test
+    void testCreateProduct() {
+        Product product = new Product();
+        product.setProductId("6f1238f8-d13a-4e5b-936f-e55156158104");
+        product.setProductName("Sampo Cap Bambang");
+        product.setProductQuantity(100);
+
+        when(productRepository.create(product)).thenReturn(product);
+
+        Product savedProduct = productServiceImpl.createProduct(product);
+        assertEquals(product.getProductId(), savedProduct.getProductId());
+        verify(productRepository, times(1)).create(product);
+    }
+
+    @Test
+    void testFindAllProduct() {
+        List<Product> productList = new ArrayList<>();
+        Product product = new Product();
+        product.setProductId("6f1238f8-d13a-4e5b-936f-e55156158104");
+        product.setProductName("Sampo Cap Bambang");
+        product.setProductQuantity(100);
+
+        Product product2 = new Product();
+        product2.setProductId("857b3c84-8eab-4296-8ca9-6773ffd86517");
+        product2.setProductName("Sampo Cap Usep");
+        product2.setProductQuantity(50);
+
+        productList.add(product);
+        productList.add(product2);
+
+        Iterator<Product> iterator = productList.iterator();
+        when(productRepository.findAll()).thenReturn(iterator);
+
+        List<Product> result = productServiceImpl.findAll();
+
+        assertEquals(productList.size(), result.size());
+        for (int i = 0; i < productList.size(); i++) {
+            assertEquals(productList.get(i), result.get(i));
+        }
+        verify(productRepository, times(1)).findAll();
+    }
+
+    @Test
+    void testFindProductById() {
+        Product product = new Product();
+        product.setProductId("6f1238f8-d13a-4e5b-936f-e55156158104");
+        product.setProductName("Sampo Cap Bambang");
+        product.setProductQuantity(100);
+        when(productRepository.findById(product.getProductId())).thenReturn(product);
+
+        Product foundProduct = productServiceImpl.findById(product.getProductId());
+
+        assertEquals(product, foundProduct);
+        verify(productRepository, times(1)).findById(product.getProductId());
+    }
+
+    @Test
+    void testEditProduct() {
+        Product product = new Product();
+        product.setProductId("6f1238f8-d13a-4e5b-936f-e55156158104");
+        product.setProductName("Sampo Cap Bambang");
+        product.setProductQuantity(100);
+        when(productRepository.edit(product)).thenReturn(product);
+
+        Product editedProduct = productServiceImpl.editProduct(product);
+
+        assertEquals(product, editedProduct);
+        verify(productRepository, times(1)).edit(product);
+    }
+
+    @Test
+    void testDeleteProduct() {
+        Product product = new Product();
+        product.setProductId("6f1238f8-d13a-4e5b-936f-e55156158104");
+        product.setProductName("Sampo Cap Bambang");
+        product.setProductQuantity(100);
+        when(productRepository.delete(product.getProductId())).thenReturn(product);
+
+        Product deletedProduct = productServiceImpl.deleteProduct(product.getProductId());
+
+        assertEquals(product, deletedProduct);
+        verify(productRepository, times(1)).delete(product.getProductId());
+    }
+}
