223a4a09eb32cb0afd25b7ffc7e96b8d07c0c2ec
DimasHN_04 <dimashn04@gmail.com>
2024-02-10 21:03:21 +0700

    Fixes for utility function - removed private constructor

--- src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EshopApplication {
-
-	private EshopApplication() {}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(EshopApplication.class, args);
