a0c7d63ad44c4d376fbc7b473f6713059ff17004
DimasHN_04 <dimashn04@gmail.com>
2024-03-01 10:25:28 +0700

    [REFACTOR] Add OrderStatus enum

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/enums/OrderStatus.java
@@ -0,0 +1,29 @@
+package id.ac.ui.cs.advprog.eshop.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum OrderStatus {
+    WAITING_PAYMENT("WAITING_PAYMENT"),
+    FAILED("FAILED"),
+    SUCCESS("SUCCESS"),
+    CANCELED("CANCELED");
+
+    private final String value;
+
+    private OrderStatus(String value) {
+        this.value = value;
+    }
+
+    public static boolean contains(String param) {
+        for (OrderStatus orderStatus : OrderStatus.values()) {
+            if (orderStatus.getValue().equals(param)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    
+}
