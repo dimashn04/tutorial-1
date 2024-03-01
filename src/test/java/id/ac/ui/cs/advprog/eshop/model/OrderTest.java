9d7c8c367867a1fe4206b06deb7170354d42e021
DimasHN_04 <dimashn04@gmail.com>
2024-03-01 09:51:17 +0700

    [RED] Add tests for Order model

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
@@ -0,0 +1,92 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.catalina.servlets.DefaultServlet.SortManager.Order;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Or;
+
+class OrderTest {
+    private List<Product> products;
+    
+    @BeforeEach
+    void setup() {
+        this.products = new ArrayList<>();
+        Product product1 = new Product();
+        product1.setProductId("e45d7d21-fd29-4533-a569-abbe0819579a");
+        product1.setProductName("Sampo Cap Bambang");
+        product1.setProductQuantity(2);
+
+        Product product2 = new Product();
+        product2.setProductId("8a76b99c-a0b3-46d2-a688-4c1831b21119");
+        product2.setProductName("Sabun Cap Usep");
+        product2.setProductQuantity(1);
+
+        this.products.add(product1);
+        this.products.add(product2);
+    }
+
+    @Test
+    void testCreateOrderEmptyProduct() {
+        this.products.clear();
+
+        assertThrows(IllegalArgumentException.class, () -> {
+           Order order = new Order("dbd4aff4-9a7f-4603-92c2-eaf529271cc9", 
+                this.products, 1708560000L, "Safira Sudrajat");
+        });
+    }
+
+    @Test
+    void testCreateOrderDefaultStatus() {
+        Order order = new Order("dbd4aff4-9a7f-4603-92c2-eaf529271cc9", 
+            this.products, 1708560000L, "Safira Sudrajat");
+
+        assertSame(this.products, order.getProducts());
+        assertEquals(2, order.getProducts().size());
+        assertEquals("Sampo Cap Bambang", order.getProducts().get(0).getProductName());
+        assertEquals("Sabun Cap Usep", order.getProducts().get(1).getProductName());
+
+        assertEquals("dbd4aff4-9a7f-4603-92c2-eaf529271cc9", order.getOrderId());
+        assertEquals(1708560000L, order.getOrderTime());
+        assertEquals("Safira Sudrajat", order.getAuthor());
+        assertEquals("WAITING_PAYMENT", order.getStatus());
+    }
+
+    @Test
+    void testCreateOrderSuccessStatus() {
+        Order order = new Order("dbd4aff4-9a7f-4603-92c2-eaf529271cc9", 
+            this.products, 1708560000L, "Safira Sudrajat", "SUCCESS");
+        assertEquals("SUCCESS", order.getStatus());
+    }
+
+    @Test
+    void testCreateOrderInvalidStatus() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Order order = new Order("dbd4aff4-9a7f-4603-92c2-eaf529271cc9", 
+                this.products, 1708560000L, "Safira Sudrajat", "MEOW");
+        });
+    }
+
+    @Test
+    void testSetStatusToCancelled() {
+        Order order = new Order("dbd4aff4-9a7f-4603-92c2-eaf529271cc9", 
+            this.products, 1708560000L, "Safira Sudrajat");
+        order.setStatus("CANCELLED");
+        assertEquals("CANCELLED", order.getStatus());
+    }
+
+    @Test
+    void testSetStatusToInvalidStatus() {
+        Order order = new Order("dbd4aff4-9a7f-4603-92c2-eaf529271cc9", 
+            this.products, 1708560000L, "Safira Sudrajat");
+        assertThrows(IllegalArgumentException.class, () -> {
+            order.setStatus("MEOW");
+        });
+    }
+}
