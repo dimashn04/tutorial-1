12256595edf93bbe7947029b1221faf8e51b34e5
DimasHN_04 <dimashn04@gmail.com>
2024-03-01 21:48:43 +0700

    [GREEN] Implement OrderRepository class

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepository.java
@@ -7,7 +7,38 @@ import id.ac.ui.cs.advprog.eshop.model.Order;
 
 public class OrderRepository {
     private List<Order> orderData = new ArrayList<>();
-    public Order save(Order order) {return null;}
-    public Order findById(String id) {return null;}
-    public List<Order> findAllByAuthor(String author) {return null;}
+
+    public Order save(Order order) {
+        int i = 0;
+        for (Order o : orderData) {
+            if (o.getId().equals(order.getId())) {
+                orderData.set(i, order);
+                return order;
+            }
+            i++;
+        }
+        
+        orderData.add(order);
+        return order;
+    }
+    public Order findById(String id) {
+        for (Order o : orderData) {
+            if (o.getId().equals(id)) {
+                return o;
+            }
+        }
+
+        return null;
+    }
+
+    public List<Order> findAllByAuthor(String author) {
+        List<Order> result = new ArrayList<>();
+        for (Order o : orderData) {
+            if (o.getAuthor().equals(author)) {
+                result.add(o);
+            }
+        }
+
+        return result;
+    }
 }
