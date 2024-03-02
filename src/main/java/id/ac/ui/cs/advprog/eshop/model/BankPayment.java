f35d638c514d98a3357231be67e95472fcae0078
DimasHN_04 <dimashn04@gmail.com>
2024-03-02 23:55:32 +0700

    [GREEN] implemented BankPayment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/BankPayment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/BankPayment.java
@@ -7,8 +7,27 @@ import lombok.Getter;
 @Getter
 public class BankPayment extends Payment {
     public BankPayment(String id, String method, Order order, Map<String, String> paymentData, String status) {
+        super(id, method, order, paymentData, status);
     }
 
     public BankPayment(String id, String method, Order order, Map<String, String> paymentData) {
+        super(id, method, order, paymentData);
+    }
+
+    @Override
+    protected void setPaymentData(Map<String, String> paymentData) {
+        if (paymentData == null || paymentData.isEmpty()) {
+            throw new IllegalArgumentException("Payment data cannot be empty");
+        }
+
+        if (paymentData.get("bankName") == null || paymentData.get("bankName").isEmpty()) {
+            throw new IllegalArgumentException("Bank name cannot be empty");
+        }
+
+        if (paymentData.get("referenceCode") == null || paymentData.get("referenceCode").isEmpty()) {
+            throw new IllegalArgumentException("Reference code cannot be empty");
+        }
+
+        this.paymentData = paymentData;
     }
 }
