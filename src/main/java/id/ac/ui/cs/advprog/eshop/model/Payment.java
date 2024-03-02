46c3ee9f7c9da7d9f05a629fd2557f42924b0cec
DimasHN_04 <dimashn04@gmail.com>
2024-03-02 19:11:54 +0700

    [GREEN] Implement Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -2,9 +2,10 @@ package id.ac.ui.cs.advprog.eshop.model;
 
 import java.util.Map;
 
+import id.ac.ui.cs.advprog.eshop.enums.PaymentMethod;
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 @Builder
 @Getter
@@ -13,12 +14,50 @@ public class Payment {
     String method;
     Order order;
     Map<String, String> paymentData;
-    @Setter
     String status;
 
     public Payment(String id, String method, Order order, Map<String, String> paymentData) {
+        this.id = id;
+        this.method = method;
+        this.order = order;
+        this.paymentData = paymentData;
+        this.status = "WAITING_PAYMENT";
+
+        if (order == null) {
+            throw new IllegalArgumentException("Order must not be null");
+        }
+
+        if (paymentData == null || paymentData.isEmpty()) {
+            throw new IllegalArgumentException("Payment data must not be null or empty");
+        }
+
+        if (method == null || method.isEmpty()) {
+            throw new IllegalArgumentException("Payment method must not be null or empty");
+        }
+
+        if (!PaymentMethod.contains(method)) {
+            throw new IllegalArgumentException("Invalid payment method");
+        }
+
+        if (method == PaymentMethod.VOUCHER.getValue() && !paymentData.containsKey("voucherCode")) {
+            throw new IllegalArgumentException("Voucher code must be provided for voucher payment");
+        }
+
+        if (method == PaymentMethod.BANK.getValue() && (!paymentData.containsKey("bankName") || !paymentData.containsKey("referenceCode"))) {
+            throw new IllegalArgumentException("Bank name and reference code must be provided for bank payment");
+        }
     }
 
     public Payment(String id, String method, Order order, Map<String, String> paymentData, String status) {
+        this(id, method, order, paymentData);
+        this.setStatus(status);
+    }
+
+    public void setStatus(String status) {
+        if (!PaymentStatus.contains(status)) {
+            throw new IllegalArgumentException("Invalid payment status");
+        }
+
+        this.status = status;
     }
 }
