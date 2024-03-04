a372023ac7effaccdc264209b7971732caacafe5
DimasHN_04 <dimashn04@gmail.com>
2024-03-04 16:48:17 +0700

    [REFACTOR] Removed unused imports

--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepositoryTest.java
@@ -3,7 +3,6 @@ package id.ac.ui.cs.advprog.eshop.repository;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.ArrayList;
 import java.util.HashMap;
