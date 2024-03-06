2569c3401b38b89821590d98a270d68591eddc8b
DimasHN_04 <dimashn04@gmail.com>
2024-03-06 20:51:38 +0700

    [REFACTOR] Set Method of DTO in postPayOrder

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/OrderController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/OrderController.java
@@ -104,7 +104,7 @@ public class OrderController {
         Map<String, String> paymentData = new HashMap<>();
         PaymentDTO paymentDTO = new PaymentDTO();
         paymentDTO.setOrderId(orderId);
-        System.out.println(payment.getMethod());
+        paymentDTO.setMethod(payment.getMethod());
 
         if (payment.getMethod().equals(PaymentMethod.VOUCHER.getValue())) {
             if (payment.getVoucherCode() == null) {
